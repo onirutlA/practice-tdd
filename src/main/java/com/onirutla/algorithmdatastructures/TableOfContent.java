6a5547a485e9a14f46ab21c6ba52f42b9026f3ef
onirutlA <alturino001@gmail.com>
2022-01-14 15:35:00 +0700

    add: todo on table of content

---
+++ src/main/java/com/onirutla/algorithmdatastructures/TableOfContent.java
@@ -0,0 +1,11 @@
+package com.onirutla.algorithmdatastructures;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TableOfContent {
+    public static List<String> tableOfContent(List<String> text) {
+//        TODO()
+        return new ArrayList<>();
+    }
+}
---
+++ src/test/java/com/onirutla/algorithmdatastructures/TableOfContentTest.java
@@ -0,0 +1,11 @@
+package com.onirutla.algorithmdatastructures;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class TableOfContentTest {
+
+//    TODO()
+}
