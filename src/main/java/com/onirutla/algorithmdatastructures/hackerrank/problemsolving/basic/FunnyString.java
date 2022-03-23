99fb17ff8e6d3510292cbf5ef8cff1dc037e74c9
onirutlA <alturino001@gmail.com>
2022-03-23 15:06:49 +0700

    add: FunnyString add: TaumBday

---
+++ src/main/java/com/onirutla/algorithmdatastructures/hackerrank/problemsolving/basic/FunnyString.java
@@ -0,0 +1,19 @@
+package com.onirutla.algorithmdatastructures.hackerrank.problemsolving.basic;
+
+public class FunnyString {
+    public static String funnyString(String s) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(s);
+        sb.reverse();
+        String rv = sb.toString();
+
+        for (int i = 0, j = 0; i < s.length() - 1 && j < sb.length() - 1; i++, j++) {
+            int sCurr = s.charAt(i), sNext = s.charAt(i + 1);
+            int rvCurr = rv.charAt(j), rvNext = rv.charAt(j + 1);
+            if (Math.abs(sCurr - sNext) != Math.abs(rvNext - rvCurr))
+                return "Not Funny";
+        }
+
+        return "Funny";
+    }
+}
