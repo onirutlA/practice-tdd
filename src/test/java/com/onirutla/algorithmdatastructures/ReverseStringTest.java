cbe2921d2abc3b82b4218b973cfd96c35932b1f6
onirutlA <alturino001@gmail.com>
2022-05-13 21:33:50 +0700

    add: ReverseString and it's test

---
+++ src/test/java/com/onirutla/algorithmdatastructures/ReverseStringTest.java
@@ -0,0 +1,13 @@
+package com.onirutla.algorithmdatastructures;
+
+import org.junit.jupiter.api.Test;
+
+import static com.onirutla.algorithmdatastructures.ReverseString.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReverseStringTest {
+    @Test
+    void testCase0(){
+        assertEquals("onirutla", reverseString4("alturino"));
+    }
+}
